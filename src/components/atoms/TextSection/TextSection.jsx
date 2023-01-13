6168ef029806df2d5bb15598035a3dfe3c8c63a9
Lukas Baksys <lubaksys@gmail.com>
2023-01-13 22:13:03 +0200

    added appear on scroll to text section

--- src/components/atoms/TextSection/TextSection.jsx
+++ src/components/atoms/TextSection/TextSection.jsx
@@ -1,24 +1,51 @@
 import { useEffect } from "react";
-import { motion } from "framer-motion";
+import { useAnimation, motion, delay } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import styles from "./TextSection.module.scss";
 
 const TextSection = () => {
+  //text appear on scroll to view
+
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const variants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 50 },
+  };
+
   return (
-    <div className={styles["text-section"]}>
+    <motion.div
+      className={styles["text-section"]}
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 0.5, delay: 0.1 }}
+      variants={variants}
+    >
       <div className={styles["title-container"]}>
         <p className={styles["title-container__title"]}>About me</p>
       </div>
       <div className={styles["content-container"]}>
         <p className={styles["content-container__text"]}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus,
-          et fugiat minus perferendis dolorem accusamus. Rerum, error. Sed rerum
-          eius maxime beatae corrupti recusandae. Eligendi odio blanditiis nisi,
-          atque iste officiis culpa modi numquam quasi non quidem soluta
-          accusantium magni commodi, ipsam aliquid harum quam debitis facilis,
-          explicabo totam tenetur!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dolor,
+          ea, temporibus inventore suscipit, in voluptate facilis neque eos modi
+          est quod voluptates expedita? Voluptas, vero doloremque molestiae
+          minima vel officiis accusamus culpa non nemo quaerat? Cumque quisquam
+          saepe cum incidunt, ipsum nam unde ea nobis? Iste corrupti earum
+          ullam?
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
