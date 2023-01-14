7fae17df24b5354e24e7f8cc4142effdefc14a06
Lukas Baksys <lubaksys@gmail.com>
2023-01-14 23:24:35 +0200

    fixing netlify err

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import styles from "./App.module.scss";
-import Navigation from "./components/atoms/navigation/Navigation";
+import Navigation from "./components/atoms/Navigation/Navigation";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/Projects";
