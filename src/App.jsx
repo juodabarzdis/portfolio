816eb074913902e39df818fe1928460b2f8c77fb
Lukas Baksys <lubaksys@gmail.com>
2023-01-14 22:19:30 +0200

    fixed import for build

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import styles from "./App.module.scss";
 import Navigation from "./components/atoms/Navigation";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/Projects";
 
